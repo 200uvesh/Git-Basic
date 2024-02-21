3623584c84adee2604916826f96133a3c1cd4f6d
Uvesh Ahmed <159881009+200uvesh@users.noreply.github.com>
2024-02-21 10:36:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,11 @@
 const mongoose = require("mongoose")
 mongoose.connect()
+//  add the "try" and "catch" block wrapper
+// try{
+//   mongoose.connect(URI)
+//   console.log("Connected Database Sucessfully")
+// }
+// catch(error){
+//   console.log("Something went wrong : Database is not Connected")
+  
+// }
